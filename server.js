0262baf4c99734b1a7e114b5e288a43dbcf9b24d
hadi112 <hadihusain112@gmail.com>
2020-05-18 01:54:05 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const puppeteer = require('puppeteer');
+const express = require('express');
+const app = express();
+
+app.get("/stats",async(req,res) => {
+    try{
+    let infourl = 'https://www.mygov.in/covid-19';
+    let browser = await puppeteer.launch({args:['--no-sandbox','--disable-setuid-sandbox']});
+    let page = await browser.newPage();
+    await page.goto(infourl, { waitUntil:'networkidle2' , timeout: 0 });
+    let data = await page.evaluate( () =>{
+        let stats = document.querySelector('div[class="information_row"]').innerText;
+        return stats;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
+    });
+    console.log(data);
+    res.send(data);
+    //alert(data);
+    await browser.close();}
+    catch(error){
+        console.log(error);
+    }
+})
+
+app.listen(3000);
